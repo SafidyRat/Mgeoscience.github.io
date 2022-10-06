b38eff9927a425bc46677954fd893e00b649af79
SafidyRat <ratolonan@gmail.com>
2022-10-06 14:23:17 +0300

    rr

--- js/datareader.js
+++ js/datareader.js
@@ -1,4 +1,4 @@
-var files=[ "./geoid/corgeoid.txt", "./geoid/EGM0825min2.txt","./geoid/wgmbouguer.txt"];
+var files=[ "./geoid/corgeoid.txt", "./geoid/EGM0825min2.txt","./geoid/wgmbouguer.txt", "./geoid/EGM2008GridData.txt"];
 
 
 var datacor=[];
@@ -13,6 +13,12 @@ var testData=[];
 var i=0;
 var addressPoints=[];
 
+var intensity=[];
+  var xgrid=[];
+  var ygrid=[];
+  var xgridl=[];
+  var ygridl=[];
+
     var reader  = new XMLHttpRequest()|| new ActiveXObject('MSXML2.XMLHTTP');
     var datacorr=[];
 
@@ -24,7 +30,7 @@ function loadcor(){
 
     reader.open("get",files[i], true);
     reader.onreadystatechange=  storedata;
-    reader.send(null);
+    //reader.send(null);
    
    }
 
@@ -104,6 +110,85 @@ if(i==2){
 }
 
 
+//////////read as grid data
+
+if(i==3){
+  
+
+
+
+
+
+  for(let j=6; j<=datacor.length;j++)
+  {
+    sp = [];
+    tosplit = String(datacor[j]); 
+
+    /*async function firstFunction(){
+      sp = tosplit.split(" ");
+      sp= sp.slice(1);
+      return;
+    };*/
+
+    sp = tosplit.split(" ");
+    sp= sp.slice(1);
+
+    //firstFunction();
+
+
+    
+    intensity.push(sp);
+    console.log( sp.length);
+
+
+  }
+
+  
+
+  //generate coordinate of grid
+
+
+
+  var s=0;
+
+  for(let j=0; j<=datacor.length-6;j++){
+    ygridl=[];
+    xgridl=[];
+    
+    for(let i=0; i<= 210;i++)
+    {
+      xgridl[i]=42.3958333+0.041666*(i);
+    
+      
+      ygridl[i]=(-1)*11.437499+(-0.04166)*s;
+
+    }
+    s=s+1;
+
+    ygrid.push(ygridl);
+
+    xgrid.push(xgridl);
+
+    //console.log(ygridl);
+
+  
+
+
+
+
+
+    //console.log("fffffff" +ygrid);
+ 
+}
+console.log( ygrid);
+console.log( "iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+
+console.log( xgrid);
+
+
+}
+
+
   
 var obj=[];
 for(let f=0; f<=datacor.length-3; f++){
@@ -111,14 +196,8 @@ for(let f=0; f<=datacor.length-3; f++){
   obj[f]={lat: Latcor[f], lng: Longcor[f], count: Cor[f]}
   Datab.push(obj[f]);
 
-  //leaflet-heat data format
-  var txt= + Cor[f];
-  //console.log(txt);
-  obj[f]=[Latcor[f],Longcor[f],txt];
-  addressPoints.push(obj[f]);
-
+  
  
-
 }
 
 
@@ -134,7 +213,7 @@ for(let f=0; f<=datacor.length-3; f++){
         data: Datab
       };
 
-      if (i<=2){
+      if (i<=3){
           i=i+1;
         loadcor();
 
@@ -142,6 +221,7 @@ for(let f=0; f<=datacor.length-3; f++){
 
     else{
         display();
+        window.open("http://127.0.0.1:5500/contour.html");
        
 
     }
